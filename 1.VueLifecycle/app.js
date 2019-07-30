51243d105b6f520dab3046772a3bfd2096e46c5c
icelove82 <yunmyeonghun@gmail.com>
2019-07-30 14:55:42 +0900

    Add lifycycle 2

--- 1.VueLifecycle/app.js
+++ 1.VueLifecycle/app.js
@@ -7,21 +7,21 @@ let vm01 = new Vue({
     };
   },
   beforeCreate() {
-    console.log("0. Event => ::beforeCreate");
+    console.log("0. Lifecycle => ::beforeCreate");
   },
   created() {
-    console.log("1. Event => ::created");
+    console.log("1. Lifecycle => ::created");
   },
   beforeMount() {
-    console.log("2. Event => ::beforeMount");
+    console.log("2. Lifecycle => ::beforeMount");
   },
   mounted() {
-    console.log("3. Event => ::mounted");
+    console.log("3. Lifecycle => ::mounted");
   },
   beforeUpdate() {
-    console.log("Event => ::beforeUpdate");
+    console.log("Lifecycle => ::beforeUpdate");
   },
   updated() {
-    console.log("Event => ::updated");
+    console.log("Lifecycle => ::updated");
   }
 });
